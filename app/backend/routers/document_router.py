6e32e673afe4e1023e41ffb330a5325da001f584
joheescu92 <107905722+joheescu92@users.noreply.github.com>
2024-12-18 11:01:11 -0300

    Delete app directory

--- app/backend/routers/document_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from services.orquestador_service import upload_document
-
-doc_router = APIRouter()
-
-@doc_router.post('/chromadb/{document_name}')
-async def upload_document_endpoint(document_name: str):
-    return upload_document(document_name)
