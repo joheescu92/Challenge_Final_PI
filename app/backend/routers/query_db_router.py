ca190ed18c44a2578a93288538e262b0e329752f
joheescu92 <107905722+joheescu92@users.noreply.github.com>
2024-12-20 10:42:35 -0300

    Delete app directory

--- app/backend/routers/query_db_router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from services.vector_database_service import consulta_db
-
-
-query_db_router = APIRouter()
-
-@query_db_router.get('/chromadb/{document_name}')
-def query_document(document_name: str):
-    return consulta_db(document_name)
